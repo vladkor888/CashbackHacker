4f84b3b5948814a75b317392145fc2d2cedfc0f1
vladkor888 <lidnn97_1997@mail.ru>
2022-06-24 08:51:39 +0300

    Bag TestNG

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -9,9 +9,9 @@ public class CashbackHackerTest {
     @org.testng.annotations.Test
     public void testRemain() {
         CashbackHacker cash = new CashbackHacker();
-        int amount = 2900;
+        int amount = 1000;
         int actual = cash.remain(amount);
-        int expected = 100;
+        int expected = 0;
         Assert.assertEquals(actual, expected);
 
     }
